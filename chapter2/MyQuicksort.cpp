21bc33c97269ec5b1a17b1ff1f720866441db934
Yohsuke Murase <flyingcamelspecial@gmail.com>
2011-04-24 23:46:13 +0900

    added a source code for problem 2-3.

---
+++ chapter2/MyQuicksort.cpp
@@ -0,0 +1,87 @@
+#include <vector>
+#include <boost/random.hpp>
+
+// ---------------------------------------
+void Swap( std::vector<int>::iterator it1, std::vector<int>::iterator it2) {
+  int temp = *it1;
+  *it1 = *it2;
+  *it2 = temp;
+}
+
+// ---------------------------------------
+void ShuffleVector( std::vector<int> &vec) {
+  boost::mt19937 zGen;  // functor which generates a random number distributing [0, 2^32-1].
+  for( size_t i=0; i<vec.size(); i++) {
+    Swap( static_cast< std::vector<int>::iterator >( &vec[i]), static_cast< std::vector<int>::iterator >( &vec[zGen() % vec.size()]) );
+    // Is there a better way than casting?
+  }
+}
+
+// -----------------------------------------
+void MyQuickSort( std::vector<int>::iterator begin, std::vector<int>::iterator end) {
+
+  if( distance( begin, end) <= 1 ) { return; }
+  
+  std::vector<int>::iterator pivot = begin;
+  // Swap( begin, pivot);   // since we select the element to be the pivot, so we do not need to swap the pivot.
+  
+  std::vector<int>::iterator it = begin;
+  it++;
+  std::vector<int>::iterator last = begin;
+  for( ; it != end; it++) {
+    if( *it <= *pivot) {
+      last++;
+      Swap( it, last);
+    }
+  }
+  Swap( pivot, last);
+  
+  MyQuickSort( begin, last);
+  MyQuickSort( ++last, end);
+}
+
+// -----------------------------------------
+void MyQuickSortParallel( std::vector<int>::iterator begin, std::vector<int>::iterator end) {
+  if( distance( begin, end) <= 1 ) { return; }
+  
+  std::vector<int>::iterator pivot = begin;
+  // Swap( begin, pivot);   // since we select the element to be the pivot, so we do not need to swap the pivot.
+  
+  std::vector<int>::iterator it = begin;
+  it++;
+  std::vector<int>::iterator last = begin;
+  for( ; it != end; it++) {
+    if( *it <= *pivot) {
+      last++;
+      Swap( it, last);
+    }
+  }
+  Swap( pivot, last);
+
+  omp_set_num_threads( 2);
+  #pragma omp parallel
+  {
+    #pragma omp sections
+    {
+      #pragma omp section
+      {
+	MyQuickSort( begin, last);
+      }
+      #pragma omp section
+      {
+	MyQuickSort( ++last, end);
+      }
+    }
+  }
+}
+
+// -----------------------------------------
+void MyBubbleSort( std::vector<int>::iterator begin, std::vector<int>::iterator end) {
+  for( std::vector<int>::iterator target = begin; target != end; target++) {
+    std::vector<int>::iterator temp_min = target;
+    for( std::vector<int>::iterator it = target; it != end; it++) {
+      if( *it < *temp_min) { temp_min = it; }
+    }
+    Swap( temp_min, target);
+  }
+}
--- chapter2/prob2-3.cpp
+++ chapter2/prob2-3.cpp
@@ -1,15 +1,15 @@
+#include "omp.h"
 #include <iostream>
 #include <vector>
 #include <algorithm>
 #include <cassert>
 #include <boost/timer.hpp>
-#include <boost/random.hpp>
+#include "MyQuicksort.cpp"
 
 double MeasureSortTime( int nArraySize);
 void PrintVector( std::vector<int> vec);
 void ShuffleVector( std::vector<int> &vec);
 void MySort( std::vector<int> & vec);
-void Swap( std::vector<int> & vec, long i, long j);
 void PrintVector( std::vector<int> vec);
 
 // ---------------------------------------
@@ -23,11 +23,10 @@ int main() {
   vecSize.push_back( 1 << 16);
   vecSize.push_back( 1 << 18);
   vecSize.push_back( 1 << 20);
-  vecSize.push_back( 1 << 22);
-  vecSize.push_back( 1 << 24);
+  // vecSize.push_back( 1 << 22);
+  // vecSize.push_back( 1 << 24);
 
-
-  for( int i=0; i<vecSize.size(); i++) {
+  for( size_t i=0; i<vecSize.size(); i++) {
     std::cout << vecSize[i] << ' ' << MeasureSortTime( vecSize[i]) * 1000 << std::endl;
   }
 
@@ -47,7 +46,10 @@ double MeasureSortTime( int nArraySize) {
   for( int i=0; i<nIteration; i++) {
     ShuffleVector( vecInt);
     zTm.restart();
-    MySort( vecInt);
+    MyQuickSort( vecInt.begin(), vecInt.end() );
+    // MyQuickSortParallel( vecInt.begin(), vecInt.end() );
+    // MyBubbleSort( vecInt.begin(), vecInt.end() );
+    sort( vecInt.begin(), vecInt.end() );
     dTotalElapsed += zTm.elapsed();
     assert( vecSorted == vecInt);
   }
@@ -59,24 +61,5 @@ void PrintVector( std::vector<int> vec) {
   for( std::vector<int>::iterator it = vec.begin(); it != vec.end(); it++) {
     std::cout << *it << std::endl;
   }
-}
-
-
-// ---------------------------------------
-void ShuffleVector( std::vector<int> &vec) {
-  boost::mt19937 zGen;  // functor which generates a random number distributing [0, 2^32-1].
-  for( long i=0; i<vec.size(); i++) { Swap( vec, i, zGen() % vec.size() ); }
-}
-
-// ---------------------------------------
-void Swap( std::vector<int> & vec, long i, long j) {
-  int temp = vec[i];
-  vec[i] = vec[j];
-  vec[j] = temp;
-}
-
-// ---------------------------------------
-void MySort( std::vector<int> & vec) {
-  std::sort( vec.begin(), vec.end() );  
 }
 
